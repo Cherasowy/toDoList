dbe376e14817597d4222d6b91aa69547d4d53865
Cherasowy <cherras@outlook.com>
2018-03-05 15:41:40 +0100

    adding todos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
-$("div").on("click",function(){
-	$(this).css("font-weight","bold");
+// thing done when clicked
+$(".todos").on("click", "li", function(){
+	$(this).toggleClass("done");
+});
+// delete li when clicked on trash
+$(".todos").on("click", "span", function(event){
+	 //no event bubbling
+	event.stopPropagation();            
+	$(this).parent().fadeOut(1000, function(){
+		$(this).remove();
+	});
+});
+//input listener
+$("input").on("keypress", function(event){
+	if (event.which == 13) {
+		$("ul").append(`<li><span><i class="fas fa-trash-alt"></i></span>${$("input").val()}</li>`);
+	}
 });
