0c7be3b6d98cfa85429bf323baa34d71f90b50d8
Cherasowy <cherras@outlook.com>
2018-03-06 09:19:06 +0100

    adding todos and little styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // thing done when clicked
-$(".todos").on("click", "li", function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("done");
 });
 // delete li when clicked on trash
-$(".todos").on("click", "span", function(event){
+$("ul").on("click", "span", function(event){
 	 //no event bubbling
 	event.stopPropagation();            
 	$(this).parent().fadeOut(1000, function(){
@@ -13,6 +13,8 @@ $(".todos").on("click", "span", function(event){
 //input listener
 $("input").on("keypress", function(event){
 	if (event.which == 13) {
-		$("ul").append(`<li><span><i class="fas fa-trash-alt"></i></span>${$("input").val()}</li>`);
+		$("ul").append(`<li><span><i class="fas fa-trash-alt"></i></span>${$(this).val()}</li>`);
+		$(this).val("");
+
 	}
 });
